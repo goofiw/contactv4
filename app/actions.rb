512fd25a84f81cbb24da90152ac9fc0bfd5da8de
goofiw <goofiw@gmail.com>
2015-06-02 20:59:15 +0000

    set up back without jason actions

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,33 @@
 # Homepage (Root path)
 get '/' do
   erb :index
+  #return all contacts and numbers
+  #search??
 end
+
+post '/contacts/new' do
+	#create new contact, add to list
+	#respond with list
+end
+
+put '/contacts/:id' do
+	#update a contact with provided json data
+end
+
+delete '/photos/:id' do
+#duh
+end
+
+post 'contacts/:id/numbers/new' do
+#add new number to contact
+end
+
+put 'contacts/:id/numbers/:number_id' do
+#not sure this works.
+end
+
+
+
+
+
+
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+  //add a div for each contact with delete, edit, and add num buttons
 
+  var info, num, functions;
+  nums = $('<div>').addClass("numbers")
+
+  for(var i = 0; i < contacts.length; i++) {
+    info = $('<div>').addClass("contact").text("Name:  " + contacts[i].first_name + " " + contacts[i].last_name + "\nEmail:  " + contacts[i].email);
+    for(var n = 0; n < contacts[i].numbers.length; n++){
+    	num = $('<div>').text(contacts[i].numbers.type + ":  " + contacts[i].numbers.number + '\n');
+    	nums.append(num)
+    }    
+  }  
+  $(info).apendto("#container");
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
